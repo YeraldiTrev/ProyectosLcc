a6eb64d3af9b4aa46467171ebf7bc02e9c8b7420
Yeraldi Treviño <ytrevino1304@gmail.com>
2022-11-01 11:46:50 -0600

    Software de Agencia A_0.0.1

--- Agencia_Autos/main.c
+++ Agencia_Autos/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+    int id;
+    char userName[30];
+    char password[30];
+    int status;
+}Users;
+
 int main()
 {
     system("clear");
+    char fileName[30]="Usuarios.bin";
+    Users user1={1001,"Yeraldi13","Lcc-2079052",1};
     return 0;
 }
